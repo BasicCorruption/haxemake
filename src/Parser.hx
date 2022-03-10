c03a5780d7cbba0dd201b1262dc7d22515ce83ce
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2022-03-09 20:47:06 -0500

    Update Parser.hx

--- src/Parser.hx
+++ src/Parser.hx
@@ -4,10 +4,16 @@ import sys.io.File;
 import sys.FileSystem;
 import haxe.io.Path;
 
+using StringTools;
+
 class Parser {
 	static var fc:String;
 	static var lines:Array<String>;
 	static var lc:Int;
+
+	static var cv = {};
+
+	static var tmp1:StringBuf;
 
 	public static function parse(fp:String) {
 		if (!FileSystem.exists(fp)) {
@@ -17,6 +23,20 @@ class Parser {
 
 			lines = fc.split("\n");
 			lc = lines.length;
+
+			for (i => v in lines) {
+				if (v.charAt(0) == "/") {
+					var t = v;
+					t.substring(2);
+					for (p in 0...t.length) {
+						if (t.charAt(p) != "\"") {
+							tmp1.addChar(p);
+						} else {
+							break;
+						}
+					}
+				}
+			}
 		}
 	}
 }
